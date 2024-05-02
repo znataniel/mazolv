f74737fe76f79bbf4ba5516833d6855d7093a6ea
znataniel <znataniel@protonmail.com>
2024-05-02 11:06:32 -0300

    cell class self.draw method added + test in main

--- src/cell.py
+++ src/cell.py
@@ -1,18 +1,22 @@
-import graphics
+from graphics import Point, Window, Line
+
 
 class Cell:
-    def __init__(self, x_1, x_2, y_1, y_2, window: graphics.Window):
-        self.l_wall = True
-        self.t_wall = True
-        self.r_wall = True
-        self.b_wall = True
+    def __init__(self, x_1, x_2, y_1, y_2, window: Window):
+        self.has_walls = [True for i in range(4)]
         self.__x_1 = x_1
         self.__x_2 = x_2
         self.__y_1 = y_1
         self.__y_2 = y_2
         self.__win = window
 
-
-    def draw(self, x, y):
-
-
+    def draw(self):
+        walls = [
+            Line(Point(self.__x_1, self.__y_1), Point(self.__x_1, self.__y_2)),
+            Line(Point(self.__x_1, self.__y_1), Point(self.__x_2, self.__y_1)),
+            Line(Point(self.__x_2, self.__y_2), Point(self.__x_2, self.__y_1)),
+            Line(Point(self.__x_2, self.__y_2), Point(self.__x_1, self.__y_2)),
+        ]
+        for i in range(len(walls)):
+            if self.has_walls[i]:
+                self.__win.draw_line(walls[i], "blue")
--- src/main.py
+++ src/main.py
@@ -5,11 +5,22 @@ from random import randrange as rr
 
 def main():
     main = Window(120, 600)
+    # --- Line drawing test
     my_line = Line(
         Point(rr(0, 120), rr(0, 600)),
         Point(rr(0, 120), rr(0, 600)),
     )
     main.draw_line(my_line, "red")
+
+    # --- Cell drawing test
+    CELL_W_H = 30
+    my_cells = []
+    for i in range(rr(1, 10)):
+        x_1, y_1 = rr(0 + CELL_W_H, 120 - CELL_W_H), rr(0 + CELL_W_H, 600 - CELL_W_H)
+        x_2, y_2 = x_1 + CELL_W_H, y_1 + CELL_W_H
+        my_cells.append(Cell(x_1, x_2, y_1, y_2, main))
+    for c in my_cells:
+        c.draw()
     main.wait_for_close()
 
 
