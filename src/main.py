945e2f5bbb64d5a0120b276687b399bcb0d72c52
znataniel <znataniel@protonmail.com>
2024-05-12 19:56:51 -0300

    add ability to draw path though cells + test

--- src/main.py
+++ src/main.py
@@ -10,17 +10,19 @@ def main():
         Point(rr(0, 120), rr(0, 600)),
         Point(rr(0, 120), rr(0, 600)),
     )
-    main.draw_line(my_line, "red")
+    main.draw_line(my_line, "green")
 
     # --- Cell drawing test
     CELL_W_H = 30
     my_cells = []
-    for i in range(rr(1, 10)):
+    for i in range(rr(4, 7)):
         x_1, y_1 = rr(0 + CELL_W_H, 120 - CELL_W_H), rr(0 + CELL_W_H, 600 - CELL_W_H)
         x_2, y_2 = x_1 + CELL_W_H, y_1 - CELL_W_H
         my_cells.append(Cell(x_1, x_2, y_1, y_2, main))
     for c in my_cells:
         c.draw()
+    for i in range(len(my_cells) - 1):
+        my_cells[i].draw_move(my_cells[i+1], bool(rr(0,2)))
     main.wait_for_close()
 
 
