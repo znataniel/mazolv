87594bf5d6f6c48d53e220b43b1997acb4a8ec6d
znataniel <znataniel@protonmail.com>
2024-05-13 11:25:08 -0300

    maze class: implement cell drawing method

--- src/main.py
+++ src/main.py
@@ -1,22 +1,16 @@
 from graphics import Window, Line, Point
+from maze import Maze
 from cell import Cell
 from random import randrange as rr
 
 
 def main():
-    main = Window(120, 600)
+    main = Window(1000, 1000)
 
-    # --- Cell drawing test
-    CELL_W_H = 30
-    my_cells = []
-    for i in range(rr(4, 7)):
-        x_1, y_1 = rr(0 + CELL_W_H, 120 - CELL_W_H), rr(0 + CELL_W_H, 600 - CELL_W_H)
-        x_2, y_2 = x_1 + CELL_W_H, y_1 - CELL_W_H
-        my_cells.append(Cell(x_1, x_2, y_1, y_2, main))
-    for c in my_cells:
-        c.draw()
-    for i in range(len(my_cells) - 1):
-        my_cells[i].draw_move(my_cells[i + 1], bool(rr(0, 2)))
+    # --- Maze drawing test
+    my_maze = Maze(100, 100, 20, 20, 30, 30, main)
+    my_maze.create_cells()
+
     main.wait_for_close()
 
 
--- src/maze.py
+++ src/maze.py
@@ -1,5 +1,11 @@
+from cell import Cell, Window
+from time import sleep
+
+
 class Maze:
-    def __init__(self, x_0, y_0, n_rows, n_cols, cell_s_x, cell_s_y, win) -> None:
+    def __init__(
+        self, x_0, y_0, n_rows, n_cols, cell_s_x, cell_s_y, win: Window
+    ) -> None:
         self.__x_0 = x_0
         self.__y_0 = y_0
         self.n_rows = n_rows
@@ -7,3 +13,26 @@ class Maze:
         self.__cell_s_x = cell_s_x
         self.__cell_s_y = cell_s_y
         self.__win = win
+
+    def create_cells(self):
+        self.cells = [
+            [
+                Cell(
+                    self.__x_0 + j * self.__cell_s_x,
+                    self.__x_0 + (j + 1) * self.__cell_s_x,
+                    self.__y_0 + i * self.__cell_s_y,
+                    self.__y_0 + (i + 1) * self.__cell_s_y,
+                    self.__win,
+                )
+                for j in range(self.n_rows)
+            ]
+            for i in range(self.n_cols)
+        ]
+
+        for row in self.cells:
+            for cell in row:
+                cell.draw()
+
+    def animate(self):
+        self.__win.redraw()
+        sleep(50e-3)
