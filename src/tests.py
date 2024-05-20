dc133b70794b887fd2b47907ab18cd99d9d8522f
znataniel <znataniel@protonmail.com>
2024-05-19 22:55:23 -0300

    add maze constructor tests

--- src/tests.py
+++ src/tests.py
@@ -10,6 +10,22 @@ class TestMaze(unittest.TestCase):
         self.assertEqual(len(m._cells[0]), n_rows)
         self.assertEqual(len(m._cells), n_columns)
 
+    def test_maze_create_cells_2(self):
+        n_rows = 0
+        n_columns = 20
+        m = Maze(0, 0, n_rows, n_columns, 10, 10)
+        self.assertEqual(len(m._cells[0]), n_rows)
+        self.assertEqual(len(m._cells), n_columns)
+
+    def test_maze_create_cells_3(self):
+        def get_first_row():
+            return m._cells[0]
+
+        n_rows = 10
+        n_columns = 0
+        m = Maze(0, 0, n_rows, n_columns, 10, 10)
+        self.assertRaises(IndexError, get_first_row)
+
 
 if __name__ == "__main__":
     unittest.main()
