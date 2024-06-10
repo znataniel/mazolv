42f91484cdd12522731ca6a2a41c05a407c6ccb0
znataniel <znataniel@protonmail.com>
2024-06-10 12:06:14 -0300

    Fix logic error that caused the maze to not create paths properly

--- src/main.py
+++ src/main.py
@@ -6,9 +6,7 @@ def main():
     main = Window(1000, 1000)
 
     # --- Maze drawing test
-    my_maze = Maze(100, 100, 20, 5, 30, 30, main)
-    my_maze._draw_cells()
-    my_maze._break_entrance_and_exit()
+    my_maze = Maze(100, 100, 15, 20, 30, 30, main)
 
     main.wait_for_close()
 
--- src/maze.py
+++ src/maze.py
@@ -10,23 +10,25 @@ class Maze:
         self._cells = []
         self._x_0 = x_0
         self._y_0 = y_0
-        self.n_rows = n_rows
         self.n_cols = n_cols
+        self.n_rows = n_rows
         self._cell_s_x = cell_s_x
         self._cell_s_y = cell_s_y
         self._win = win
         self._create_cells()
+        self._break_entrance_and_exit()
+        self._break_walls_rec(0, 0)
         random.seed(seed)
 
     def _create_cells(self):
         self._cells = [
-            [Cell(self._win) for j in range(self.n_rows)] for i in range(self.n_cols)
+            [Cell(self._win) for j in range(self.n_cols)] for i in range(self.n_rows)
         ]
         self._draw_cells()
 
     def _draw_cells(self):
-        for i in range(self.n_cols):
-            for j in range(self.n_rows):
+        for i in range(self.n_rows):
+            for j in range(self.n_cols):
                 self._draw_single_cell(i, j)
 
     def _draw_single_cell(self, i, j):
@@ -36,42 +38,45 @@ class Maze:
             self._y_0 + i * self._cell_s_y,
             self._y_0 + (i + 1) * self._cell_s_y,
         )
+        self.animate()
 
     def animate(self):
         if self._win:
             self._win.redraw()
-        sleep(50e-3)
+        sleep(10e-3)
 
     def _break_entrance_and_exit(self):
         entrance_cell = self._cells[0][0]
-        exit_cell = self._cells[self.n_cols - 1][self.n_rows - 1]
+        exit_cell = self._cells[self.n_rows - 1][self.n_cols - 1]
         entrance_cell.has_walls[1] = False
         exit_cell.has_walls[3] = False
         self._draw_single_cell(0, 0)
-        self._draw_single_cell(self.n_cols - 1, self.n_rows - 1)
+        self._draw_single_cell(self.n_rows - 1, self.n_cols - 1)
 
     def _break_walls_rec(self, i, j):
         self._cells[i][j].visited = True
         adjacents = [
-            [i - 1, j],  # Left
-            [i, j - 1],  # Top
-            [i + 1, j],  # Right
-            [i, j + 1],  # Bottom
+            (i, j - 1),  # Left
+            (i - 1, j),  # Top
+            (i, j + 1),  # Right
+            (i + 1, j),  # Bottom
         ]
+
         adjacents_possible = []
         for adjacent in adjacents:
-            if self._cells[adjacent[0]][adjacent[1]].visited:
+            if adjacent[0] in range(self.n_rows) and adjacent[1] in range(self.n_cols):
                 adjacents_possible.append(adjacent)
-        while 1:
-            if not adjacents_possible:
-                self._draw_single_cell(i, j)
-                return
+
+        while adjacents_possible:
             chosen = random.choice(adjacents_possible)
-            side = adjacents.index(chosen)
-            self._cells[i][j].has_walls[side]
-            self._cells[chosen[0]][chosen[1]].has_walls[side - 2]
-            self._break_walls_rec(chosen[0], chosen[1])
+            if not self._cells[chosen[0]][chosen[1]].visited:
+                side = adjacents.index(chosen)
+                self._cells[i][j].has_walls[side] = False
+                self._cells[chosen[0]][chosen[1]].has_walls[side - 2] = False
+                self._break_walls_rec(chosen[0], chosen[1])
             adjacents_possible.remove(chosen)
+
+        self._draw_single_cell(i, j)
 
     def _reset_cells_visited(self):
         for row in self._cells:
